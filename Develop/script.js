320842256d0f75eba856e49a85bfad5169f94ce3
Ben Brumby <bbrumby00@gmail.com>
2020-09-10 22:35:17 -0400

    Began working with the study group to brainstorm ideas

--- Develop/script.js
+++ Develop/script.js
@@ -1,111 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 var selections = [];
 
-var userOptions = {
+var numberofChars
+
+var userSelections = {
       includeLowercase : false,
       includeUppercase : false,
       includeNumbers : false,
       includeSymbols : false,
+}  
 
-    userInputs: function () {
-      var lowercaseChoice = confirm("Should we include lowercase letters?") 
+function generatePassword () {
 
-      if (this.includeLowercase = true)
-          selections.push(lowercase)
+  var charNumber = prompt("How many characters would you like?");
 
-      var uppercaseChoice = confirm("Should we include uppercase letters?")
+  confirm("Should we include lowercase letters?"); 
+          this.includeLowercase = true;
+          selections.push(lowercase);
+          console.log("Lowercase: " + userOptions.includeLowercase);
+
+  confirm("Should we include uppercase letters?");
       if (this.includeUppercase = true)
-          selections.push(uppercase)
+          selections.push(uppercase);
+          console.log("Uppercase: " + userOptions.includeUppercase);
 
-      var numbersChoice = confirm("Should we include numbers?")
+  confirm("Should we include numbers?");
       if (this.includeNumbers = true)
-          selections.push(numbers)
+          selections.push(numbers);
+          console.log("Numbers: " + userOptions.includeNumbers);
 
-      var symbolsChoice = confirm("Should we include symbols?")
+  confirm("Should we include symbols?");
       if (this.includesymbols = true)
-         selections.push(symbols) 
-    }
-  }
-
+         selections.push(symbols);
+         console.log("Symbols: " + userOptions.includeSymbols)
+}
 
 // Write password to the #password input
 
 function writePassword() {
   var password = generatePassword();
-
-function choiceLowercase() {
-  if (confirm("Should we include lowercase letters?)) {
-
-  }
-
-    if (choiceLowercase === "yes") {
-      selections.push(lowercase);
-      alert("Ok. We will include lowercase letters.");}
-
-    if (choiceLowercase === "no");
-    
-
- 
-
-    choiceUppercase = prompt("Do you wish to include uppercase letters? Answer 'yes' or 'no'.");
-    choiceUppercase = choiceUppercase.toLowerCase();
-  
-      if (choiceUppercase == "yes") {
-        selections.push(uppercase);
-        alert("Ok. We will include uppercase letters.");}
-  
-      if (choiceUppercase == "no") 
-        alert("Ok. We will not include uppercase letters.");
-  
-      else 
-       alert("That is not a valid option.")
-          
-
-    
-    choiceNumber = prompt("Do you wish to include numbers? Answer 'yes' or 'no'.");
-    
-      if (choiceNumber == "yes") {
-          selections.push(numbers);
-          alert("Ok. We will include numbers.");}
-    
-      if (choiceNumber == "no") 
-        alert("Ok. We will not include numbers.");
-    
-      else 
-        alert("That is not a valid option.");
-      
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+}
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
