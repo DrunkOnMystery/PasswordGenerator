af309e73387eacf0cffe905be0d24b1ccb87b3fc
Ben Brumby <bbrumby00@gmail.com>
2020-09-10 20:52:07 -0400

    Trying different things with the code. Not making much progress

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var selections = {
+    includeLowercase : false,
+    includeUppercase : false,
+    includeNumbers : false,
+    includeSymbols : false,
+}
+
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var symbol = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-var selections = [];
 
 
+var lowercaseChoice = confirm("Should we include lowercase letters?") 
+
+    if (this.includeLowercase = true)
+    
+var uppercaseChoice = confirm("Should we include uppercase letters?")
+    if (this.includeUppercase = true)
+
+var numbersChoice = confirm("Should we include numbers?")
+    if (this.includeNumbers = true)
+
+var symbolsChoice = confirm("Should we include symbols?")
+    if (this.includesymbols = true)
 
 // Write password to the #password input
 
 function writePassword() {
   var password = generatePassword();
 
-  while (choiceLowercase !== null) {
+function choiceLowercase() {
+  if (confirm("Should we include lowercase letters?)) {
 
-  choiceLowercase = prompt("Do you wish to include lowercase letters? Answer 'yes' or 'no'.");
-  choiceLowercase = choiceLowercase.toLowerCase();
-
-    if ((choiceLowercase !== "yes") && (choiceLowercase !== "no")); {
-      alert("That is not a valid option."); }
+  }
 
     if (choiceLowercase === "yes") {
       selections.push(lowercase);
       alert("Ok. We will include lowercase letters.");}
 
-    else (choiceLowercase === "no") 
-      alert("Ok. We will not include lowercase letters.");
-    }
+    if (choiceLowercase === "no");
+    
 
-    while (choiceUppercase !== null) {  
+ 
 
     choiceUppercase = prompt("Do you wish to include uppercase letters? Answer 'yes' or 'no'.");
     choiceUppercase = choiceUppercase.toLowerCase();
@@ -44,9 +58,9 @@ function writePassword() {
   
       else 
        alert("That is not a valid option.")
-      }    
+          
 
-      while (choiceNumber !== null) {  
+    
     choiceNumber = prompt("Do you wish to include numbers? Answer 'yes' or 'no'.");
     
       if (choiceNumber == "yes") {
@@ -57,8 +71,8 @@ function writePassword() {
         alert("Ok. We will not include numbers.");
     
       else 
-        alert("That is not a valid option.")
-      }  
+        alert("That is not a valid option.");
+      
   
 
 
